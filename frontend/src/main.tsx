b71fee13cbae5908f7fb9a21512287b62cae3078
Yukih <4rnold.richard@gmail.com>
2025-05-25 21:37:50 +0300

    Login & Signup page designed

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,8 +3,15 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+const rootElement = document.getElementById("root");
+
+if (!rootElement) {
+  console.log("Root element not found");
+} else {
+    const root = createRoot(rootElement);
+    root.render (
+        <StrictMode>
+            <App />
+        </StrictMode>
+    );
+}
